5598bfea6d3831a4199a3ab840579b75740f8f90
Zane-dev16 <irellzanedev@gmail.com>
2021-03-13 21:22:02 +0000

    create registration

---
+++ wealth_awesomeness/registration/views.py
@@ -0,0 +1,12 @@
+from django.shortcuts import render
+from django.contrib.auth.forms import UserCreationForm
+
+
+def register(request):
+    if request.method == 'POST':
+        form = UserCreationForm(request.POST)
+        if form.is_valid():
+            form.save()
+    else:
+        form = UserCreationForm()
+    return render(request, 'registration/register.html', {'form': form})
